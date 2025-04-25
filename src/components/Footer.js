84787b5b56fcea05891f153462e4239f14b59bc0
Mostafa-3ta <m2ostafa2000@gmail.com>
2025-04-26 02:52:17 +0300

    edit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,18 @@ import { Box } from '@mui/material'
 import React from 'react'
 
 export default function Footer() {
+
+    const Links = [
+        { link: "https://github.com/Mostafa3ta", icon: "fab fa-github" },
+        { link: "https://www.facebook.com/profile.php?id=100007766405910&ref=xav_ig_profile_web", icon: "fa-solid fa-link" },
+        { link: "https://www.linkedin.com/in/mostafa-mahmoud-33a1542b0", icon: "fab fa-linkedin-in" }
+    ]
     return <>
         <Box className='footer' >
-                <a href="https://www.facebook.com/profile.php?id=100007766405910&ref=xav_ig_profile_web" target='_blank' rel='noopener noreferrer'>
-                    <i className="fab fa-facebook"></i></a>
-
-                <a href="https://github.com/Mostafa3ta" target='_blank' rel='noopener noreferrer'>
-                    <i className="fab fa-github"></i></a>
-
-                <a href="https://www.linkedin.com/in/mostafa-mahmoud-33a1542b0" target="_blank" rel='noopener noreferrer'>
-                    <i className="fab fa-linkedin-in"></i>
-                </a>
+            {Links.map((link, index) =>
+                <a key={index} href={link.link} target='_blank' rel='noopener noreferrer'>
+                    <i className={link.icon}></i></a>
+            )}
         </Box>
     </>
 }
